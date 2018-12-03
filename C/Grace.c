0f2c80235ee7cc1fce236d4af2f92300fbe0387a
Guilhem DE LA BROSSE <gdelabro@e1r8p5.42.fr>
2018-12-03 17:09:20 +0100

    all exo working

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* com */
-#define STR "#include <stdio.h>%c/* com */%c#define STR %c%s%c%c#define WRITE_FILE(x) fprintf(x,STR,10,10,34,STR,34,10,10,34,34,34,34,10,10);%c#define MAIN(x)int main(){FILE *file = fopen(%cGrace_kid.c%c, %cr+%c);WRITE_FILE(file);}%cMAIN(0)%c"
+#define STR "#include <stdio.h>%c/* com */%c#define STR %c%s%c%c#define WRITE_FILE(x) fprintf(x,STR,10,10,34,STR,34,10,10,34,34,34,34,10,10);%c#define MAIN(x)int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw+%c);WRITE_FILE(file);fclose(file);}%cMAIN(0)%c"
 #define WRITE_FILE(x) fprintf(x,STR,10,10,34,STR,34,10,10,34,34,34,34,10,10);
-#define MAIN(x)int main(){FILE *file = fopen("Grace_kid.c", "r+");WRITE_FILE(file);}
+#define MAIN(x)int main(){FILE *file = fopen("Grace_kid.c", "w+");WRITE_FILE(file);fclose(file);}
 MAIN(0)
